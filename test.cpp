e5e9d5bb8bba31593800c085b83d8ce089e5769b
Jeff Chen <kevinisjeff@berkeley.edu>
2020-03-04 19:02:24 -0800

    test

--- test.cpp
+++ test.cpp
@@ -2,21 +2,29 @@
 #include <librealsense2/rs.hpp>
 #include "../cv-helpers.hpp"
 
-auto data = pipe.wait_for_frames();
-data = align_to.process(data);
+int main( int argc, char** argv )
+{
+    auto data = pipe.wait_for_frames();
+    data = align_to.process(data);
 
-auto color_frame = data.get_color_frame();
-auto depth_frame = data.get_depth_frame();
+    auto color_frame = data.get_color_frame();
+    auto depth_frame = data.get_depth_frame();
 
-auto color_mat = frame_to_mat(color_frame);
-auto depth_mat = frame_to_mat(depth_frame);
+    auto color_mat = frame_to_mat(color_frame);
+    auto depth_mat = frame_to_mat(depth_frame);
 
-//! [window]
-namedWindow( "color", WINDOW_AUTOSIZE );
-//! [window]
-//! [window]
-namedWindow( "depth", WINDOW_AUTOSIZE );
-//! [window]
+    //! [window]
+    namedWindow( "color", WINDOW_AUTOSIZE );
+    //! [window]
+    //! [window]
+    namedWindow( "depth", WINDOW_AUTOSIZE );
+    //! [window]
 
-imshow("color",color_mat);
-imshow("depth",depth_mat);
+    imshow("color",color_mat);
+    imshow("depth",depth_mat);
+
+    //! [wait]
+    waitKey(0); // Wait for a keystroke in the window
+    //! [wait]
+    return 0;
+}
