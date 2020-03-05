661090beacadd919ad0b35db5fedb00efae7cbfb
Jeff Chen <kevinisjeff@berkeley.edu>
2020-03-04 19:00:18 -0800

    test script ready for deployment

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/dnn.hpp>
+#include <librealsense2/rs.hpp>
+#include "../cv-helpers.hpp"
+
+auto data = pipe.wait_for_frames();
+data = align_to.process(data);
+
+auto color_frame = data.get_color_frame();
+auto depth_frame = data.get_depth_frame();
+
+auto color_mat = frame_to_mat(color_frame);
+auto depth_mat = frame_to_mat(depth_frame);
+
+//! [window]
+namedWindow( "color", WINDOW_AUTOSIZE );
+//! [window]
+//! [window]
+namedWindow( "depth", WINDOW_AUTOSIZE );
+//! [window]
+
+imshow("color",color_mat);
+imshow("depth",depth_mat);
