b7c5c93c10f0cac46d5f9cf7b151203a47d9dd1e
Jeff Chen <kevinisjeff@berkeley.edu>
2020-03-05 16:04:59 -0800

    added left infrared frame

--- test.cpp
+++ test.cpp
@@ -1,18 +1,23 @@
+//! [includes]
 #include <opencv2/dnn.hpp>
 #include <librealsense2/rs.hpp>
 #include "../cv-helpers.hpp"
+
 #include <iostream>
+//! [includes]
 
 int main( int argc, char** argv )
 {
-    auto data = pipe.wait_for_frames();
-    data = align_to.process(data);
+    auto data = pipe.wait_for_frames(); // Wait for next set of frames
+    data = align_to.process(data); // Align color frame to depth import
 
-    auto color_frame = data.get_color_frame();
-    auto depth_frame = data.get_depth_frame();
+    auto color_frame = data.get_color_frame(); // Get color frame
+    auto depth_frame = data.get_depth_frame(); // Get depth frame
+    auto infrared_frame = data.get_infrared_frame(1); // get left infrared frame
 
     auto color_mat = frame_to_mat(color_frame);
     auto depth_mat = frame_to_mat(depth_frame);
+    auto infrared_mat = frame_to_mat(infrared_frame)
 
     //! [window]
     namedWindow( "color", WINDOW_AUTOSIZE );
@@ -20,9 +25,13 @@ int main( int argc, char** argv )
     //! [window]
     namedWindow( "depth", WINDOW_AUTOSIZE );
     //! [window]
+    //! [window]
+    namedWindow( "infrared", WINDOW_AUTOSIZE );
+    //! [window]
 
     imshow("color",color_mat);
     imshow("depth",depth_mat);
+    imshow("infrared",depth_mat);
 
     //! [wait]
     waitKey(0); // Wait for a keystroke in the window
